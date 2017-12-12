11adeb6c7490de1ebb4d1fc1bc7ffa7a6733829c
Kövesi Patrik <kovesipatrik@gmail.com>
2017-12-12 17:03:54 +0100

    web first commit

---
+++ src/view/main.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Kövesi Patrik</title>
+    <!--<link href="style.css" type="text/css" rel="stylesheet">-->
+    <!--    <link href="bootstrap.css" type="text/css" rel="stylesheet">-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="../view/style0.css" type="text/css" rel="stylesheet">
+
+</head>
+<body>
+
+
+<!--fejléc-->
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-1">
+            <div class="fejléc-logo"><a href=""><img src="../images/logo-fehér.png" style="height:64px"></a></div>
+        </div>
+        <div class="col-3">
+            <div class="fejléc-név">Kövesi Patrik</div>
+        </div>
+        <div class="col-3"></div>
+        <div class="col-5">
+            <a href="../view/main.php">Kezdőlap / </a>
+            <a href="../view/projects.php">Munkáim  / </a>
+            <a href="../view/contact.html">Kapcsolat / </a>
+            <a href="../view/about.html">Rólam</a>
+        </div>
+    </div>
+</div>
+
+<div class="választóvonal">
+    <hr>
+</div>
+
+<!--szövegtörzs-->
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h1 class="szövegtörzscim"><u>Fejlesztések:</u></h1>
+        </div>
+        <div class="col-4">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <div style="text-align: center">
+                <div><img src="../images/Trench-xwing-attack.jpg"></div>
+                <div class="hircim"><b>Közeleg a Halálcsillag támadása!</b></div>
+                <div class="hirtéma">Wars of the Galaxy</div>
+                <div class="hirdátum">2017.12.10</div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div>
+                <ul class="cuccoklinkek"><b>Linkek</b>
+                    <li><a href="../view/main.php">Kezdőlap</a></li>
+                    <li><a href="../view/projects.php">Munkáim</a></li>
+                    <li><a href="../view/contact.html">Kapcsolat</a></li>
+                    <li><a href="../view/about.html">Rólam</a></li>
+                </ul>
+                <div class="választóvonal">
+                    <hr>
+                </div>
+                <ul class="cuccoklinkek"><b>Linkek</b>
+                    <li><a href="../view/main.php">Kezdőlap</a></li>
+                    <li><a href="../view/projects.php">Munkáim</a></li>
+                    <li><a href="../view/contact.html">Kapcsolat</a></li>
+                    <li><a href="../view/about.html">Rólam</a></li>
+                </ul>
+                <div class="választóvonal">
+                    <hr>
+                </div>
+                <ul class="cuccoklinkek"><b>Linkek</b>
+                    <li><a href="../view/main.php">Kezdőlap</a></li>
+                    <li><a href="../view/projects.php">Munkáim</a></li>
+                    <li><a href="../view/contact.html">Kapcsolat</a></li>
+                    <li><a href="../view/about.html">Rólam</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-8">
+            <div style="text-align: center">
+                <div><img src="../images/Trench-xwing-attack.jpg"></div>
+                <div class="hircim"><b>Közeleg a Halálcsillag támadása!</b></div>
+                <div class="hirtéma">Wars of the Galaxy</div>
+                <div class="hirdátum">2017.12.10</div>
+            </div>
+        </div>
+    </div>
+    <div class="row align-items-center">
+        <div class="col-8">
+            <div style="text-align: center">
+                <div><img src="../images/Trench-xwing-attack.jpg"></div>
+                <div class="hircim"><b>Közeleg a Halálcsillag támadása!</b></div>
+                <div class="hirtéma">Wars of the Galaxy</div>
+                <div class="hirdátum">2017.12.10</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="választóvonal"><hr></div>
+<!--lábléc-->
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col">
+            <div class="lábléc-infok">
+                <ul>
+                    <li>Név: Kövesi Patrik</li>
+                    <li>tel: 06-39/123-4567</li>
+                    <li>Email: kovesipatrik@emailcim.com</li>
+                    <li>fb: kovesipatrik.link</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <div class="lábléc-középső">
+                <ul>
+                    <li>Készítette: <b>Kövesi Patrik</b></li>
+                    <li>2017.12.XX</li>
+                    <li>Minden jog fentartva stb stb!</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <div class="lábléc-infok">
+                <ul>
+                    <li>Név: Kövesi Patrik</li>
+                    <li>tel: 06-39/123-4567</li>
+                    <li>Email: kovesipatrik@emailcim.com</li>
+                    <li>fb: kovesipatrik.link</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!--bootstrap scriptek-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+</body>
+</html>
